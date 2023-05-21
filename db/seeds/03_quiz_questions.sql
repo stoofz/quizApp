96151153c35460dc6df20185d07cd0aa29a517dd
Anthony Chambers <ajfchambers@gmail.com>
2023-05-20 20:43:20 -0400

    Create seed data

--- db/seeds/03_quiz_questions.sql
+++ db/seeds/03_quiz_questions.sql
@@ -1,17 +1,17 @@
-INSERT INTO quiz_questions (quiz_id, question, answer) VALUES (1, 'What port is Secure Shell SSH?', '22');
-INSERT INTO quiz_questions (quiz_id, question, answer) VALUES (1, 'What port is Domain Name System DNS?', '53');
-INSERT INTO quiz_questions (quiz_id, question, answer) VALUES (1, 'What port is Hyper Text Transfer Protocol Secure HTTPS?', '443');
-INSERT INTO quiz_questions (quiz_id, question, answer) VALUES (1, 'What port is Simple Mail Transfer Protocol SMTP?', '25');
-INSERT INTO quiz_questions (quiz_id, question, answer) VALUES (1, 'What port is File Transfer Protocol? FTP', '21');
+INSERT INTO quiz_questions (quiz_id, question) VALUES (1, 'What port is Secure Shell SSH?');
+INSERT INTO quiz_questions (quiz_id, question) VALUES (1, 'What port is Domain Name System DNS?');
+INSERT INTO quiz_questions (quiz_id, question) VALUES (1, 'What port is Hyper Text Transfer Protocol Secure HTTPS?');
+INSERT INTO quiz_questions (quiz_id, question) VALUES (1, 'What port is Simple Mail Transfer Protocol SMTP?');
+INSERT INTO quiz_questions (quiz_id, question) VALUES (1, 'What port is File Transfer Protocol? FTP');
 
-INSERT INTO quiz_questions (quiz_id, question, answer) VALUES (2, 'What year was The Rock released?', '1996');
-INSERT INTO quiz_questions (quiz_id, question, answer) VALUES (2, 'What year was Pulp Fiction Released?', '1994');
-INSERT INTO quiz_questions (quiz_id, question, answer) VALUES (2, 'What year was The Matrix released?', '1999');
-INSERT INTO quiz_questions (quiz_id, question, answer) VALUES (2, 'What year was Training Day released?', '2001');
-INSERT INTO quiz_questions (quiz_id, question, answer) VALUES (2, 'What year was Fargo released?', '1996');
+INSERT INTO quiz_questions (quiz_id, question) VALUES (2, 'What year was The Rock released?');
+INSERT INTO quiz_questions (quiz_id, question) VALUES (2, 'What year was Pulp Fiction Released?');
+INSERT INTO quiz_questions (quiz_id, question) VALUES (2, 'What year was The Matrix released?');
+INSERT INTO quiz_questions (quiz_id, question) VALUES (2, 'What year was Training Day released?');
+INSERT INTO quiz_questions (quiz_id, question) VALUES (2, 'What year was Fargo released?');
 
-INSERT INTO quiz_questions (quiz_id, question, answer) VALUES (3, 'Whats the capital of Ontario?', 'Toronto');
-INSERT INTO quiz_questions (quiz_id, question, answer) VALUES (3, 'Whats the capital of British Columbia?', 'Victoria');
-INSERT INTO quiz_questions (quiz_id, question, answer) VALUES (3, 'Whats the capital of Colorado?', 'Denver');
-INSERT INTO quiz_questions (quiz_id, question, answer) VALUES (3, 'Whats the capital of England?', 'London');
-INSERT INTO quiz_questions (quiz_id, question, answer) VALUES (3, 'Whats the captial of Canada?', 'Ottawa');
+INSERT INTO quiz_questions (quiz_id, question) VALUES (3, 'Whats the capital of Ontario?');
+INSERT INTO quiz_questions (quiz_id, question) VALUES (3, 'Whats the capital of British Columbia?');
+INSERT INTO quiz_questions (quiz_id, question) VALUES (3, 'Whats the capital of Colorado?');
+INSERT INTO quiz_questions (quiz_id, question) VALUES (3, 'Whats the capital of England?');
+INSERT INTO quiz_questions (quiz_id, question) VALUES (3, 'Whats the captial of Canada?');
---
+++ db/seeds/04_quiz_answers.sql
@@ -0,0 +1,75 @@
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (1, '25', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (1, '21', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (1, '80', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (1, '22', TRUE);
+
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (2, '443', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (2, '587', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (2, '8080', FALSE);
+INSERT INTO quiz_answers (quiz_asnwer_id, answer, correct) VALUES (2, '53', TRUE);
+
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (3, '993', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (3, '443', FALSE);
+INSERT INTO quiz_answers (quiz_asnwer_id, answer, correct) VALUES (3, '25', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (3, '22', TRUE);
+
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (4, '25', TRUE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (4, '547', FALSE);
+INSERT INTO quiz_answers (quiz_asnwer_id, answer, correct) VALUES (4, '1337', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (4, '22', FALSE);
+
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (5, '21 or 20', TRUE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (5, '8787', FALSE);
+INSERT INTO quiz_answers (quiz_asnwer_id, answer, correct) VALUES (5, '1337', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (5, 'All the above', FALSE);
+
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (6, '1996', TRUE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (6, '2000', FALSE);
+INSERT INTO quiz_answers (quiz_asnwer_id, answer, correct) VALUES (6, '1927', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (6, '2020', FALSE);
+
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (7, '2000', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (7, '2001', FALSE);
+INSERT INTO quiz_answers (quiz_asnwer_id, answer, correct) VALUES (7, '1994', TRUE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (7, '2002', FALSE);
+
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (8, '2021', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (8, '1981', FALSE);
+INSERT INTO quiz_answers (quiz_asnwer_id, answer, correct) VALUES (8, '1977', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (8, '1999', TRUE);
+
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (9, '2001', TRUE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (9, '2023', FALSE);
+INSERT INTO quiz_answers (quiz_asnwer_id, answer, correct) VALUES (9, '1997', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (9, '1967', FALSE);
+
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (10, '1980', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (10, '1996', TRUE);
+INSERT INTO quiz_answers (quiz_asnwer_id, answer, correct) VALUES (10, '1981', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (10, '1988', FALSE);
+
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (11, 'Toronto', TRUE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (11, 'New York', FALSE);
+INSERT INTO quiz_answers (quiz_asnwer_id, answer, correct) VALUES (11, 'San Francisco', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (11, 'Tire', FALSE);
+
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (12, 'Tennis Ball', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (12, 'Mexico', FALSE);
+INSERT INTO quiz_answers (quiz_asnwer_id, answer, correct) VALUES (12, 'Victoria', TRUE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (12, 'Quebec', FALSE);
+
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (13, 'Fishing Rod', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (13, 'Total Recall', FALSE);
+INSERT INTO quiz_answers (quiz_asnwer_id, answer, correct) VALUES (13, 'Number One', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (13, 'Denver', TRUE);
+
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (14, 'London', TRUE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (14, 'Peru', FALSE);
+INSERT INTO quiz_answers (quiz_asnwer_id, answer, correct) VALUES (14, 'Florida', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (14, 'USA', FALSE);
+
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (15, 'California', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (15, 'Ottawa', TRUE);
+INSERT INTO quiz_answers (quiz_asnwer_id, answer, correct) VALUES (15, 'The Moon', FALSE);
+INSERT INTO quiz_answers (quiz_answer_id, answer, correct) VALUES (15, 'Venus', FALSE);
+
