1132a3394f990dabb51495a4b5d31d5b741027a1
Anthony Chambers <ajfchambers@gmail.com>
2023-05-20 22:44:29 -0400

    Clean for eslint

--- public/scripts/users.js
+++ public/scripts/users.js
@@ -1,17 +1,19 @@
 // Client facing scripts here
+/* eslint-env jquery */
+
 $(() => {
   $('#fetch-users').on('click', () => {
     $.ajax({
       method: 'GET',
       url: '/api/users'
     })
-    .done((response) => {
-      const $usersList = $('#users');
-      $usersList.empty();
+      .done((response) => {
+        const $usersList = $('#users');
+        $usersList.empty();
 
-      for(const user of response.users) {
-        $(`<li class="user">`).text(user.name).appendTo($usersList);
-      }
-    });
+        for (const user of response.users) {
+          $(`<li class="user">`).text(user.name).appendTo($usersList);
+        }
+      });
   });
 });
