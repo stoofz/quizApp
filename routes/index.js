0f877e082f5b08c11f822c7a73395f2a68c89121
Anthony Chambers <ajfchambers@gmail.com>
2023-05-21 15:17:25 -0400

    Create initial home route

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db/connection');
+
+router.get('/', (req, res) => {
+  db.query(`
+  SELECT *
+  FROM quizzes
+  WHERE public = TRUE;
+  `)
+    .then(data => {
+      const templateVar = { quizzes: data.rows };
+      res.render('../views/index', templateVar);
+    });
+});
+
+module.exports = router;
+
