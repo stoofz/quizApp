9e129d70426484e118bc9940af589c7a0161e24d
Anthony Chambers <ajfchambers@gmail.com>
2023-05-20 20:43:42 -0400

    Adjust columns

--- db/schema/03_quiz_questions.sql
+++ db/schema/03_quiz_questions.sql
@@ -3,6 +3,5 @@ DROP TABLE IF EXISTS quiz_questions CASCADE;
 CREATE table quiz_questions (
   id SERIAL PRIMARY KEY NOT NULL,
   quiz_question_id INTEGER REFERENCES quizzes(id) ON DELETE CASCADE,
-  question VARCHAR(255) NOT NULL,
-  answer VARCHAR(255) NOT NULL
+  question VARCHAR(255) NOT NULL
 );
---
+++ db/schema/04_quiz_answers.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS quiz_answers CASCADE;
+
+CREATE TABLE quiz_answers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  quiz_answer_id INTEGER REFERENCES quiz_questions(id) ON DELETE CASCADE,
+  quiz_answer VARCHAR(255) NOT NULL,
+  correct BOOLEAN NOT NULL DEFAULT FALSE
+);
