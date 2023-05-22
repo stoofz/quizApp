b2adaa63071cf8b78947722567064e9ce2b15aff
Anthony Chambers <ajfchambers@gmail.com>
2023-05-22 14:33:56 -0400

    Modify to bcrypted 'password'

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (name, email, password) VALUES ('Pete Mo', 'mpo@mail.com', 'password');
-INSERT INTO users (name, email, password) VALUES ('Lyda Feep', 'feep@mail.com', 'password');
-INSERT INTO users (name, email, password) VALUES ('Todd Bangaran', 'tbang@mail.com', 'password');
-INSERT INTO users (name, email, password) VALUES ('George Till', 'gtill@mail.com', 'password');
-INSERT INTO users (name, email, password) VALUES ('Tyson Meo', 'tmeo@mail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Pete Mo', 'mpo@mail.com', '$2b$10$bnA3fIy7jw6A4z15qmMmS.aoFVRUoT8FROOn1DyO8oDywYrVEas4e');
+INSERT INTO users (name, email, password) VALUES ('Lyda Feep', 'feep@mail.com', '$2b$10$bnA3fIy7jw6A4z15qmMmS.aoFVRUoT8FROOn1DyO8oDywYrVEas4e');
+INSERT INTO users (name, email, password) VALUES ('Todd Bangaran', 'tbang@mail.com', '$2b$10$bnA3fIy7jw6A4z15qmMmS.aoFVRUoT8FROOn1DyO8oDywYrVEas4e');
+INSERT INTO users (name, email, password) VALUES ('George Till', 'gtill@mail.com', '$2b$10$bnA3fIy7jw6A4z15qmMmS.aoFVRUoT8FROOn1DyO8oDywYrVEas4e');
+INSERT INTO users (name, email, password) VALUES ('Tyson Meo', 'tmeo@mail.com', '$2b$10$bnA3fIy7jw6A4z15qmMmS.aoFVRUoT8FROOn1DyO8oDywYrVEas4e');
