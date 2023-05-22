25cde54ff10a5e35799b8d5d94f61ac6ba13fbb7
Anthony Chambers <ajfchambers@gmail.com>
2023-05-21 23:21:38 -0400

    Add comments

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/connection');
 
-
+// Display all public quizzes
 router.get('/', (req, res) => {
   db.query(`
   SELECT *
