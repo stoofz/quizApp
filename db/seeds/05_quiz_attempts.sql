6f6f9ca0f21c333c8e12ec623bb1d81650af3620
Anthony Chambers <ajfchambers@gmail.com>
2023-05-23 21:36:35 -0400

    Adjust seed data

--- db/seeds/05_quiz_attempts.sql
+++ db/seeds/05_quiz_attempts.sql
@@ -1,6 +1,5 @@
-INSERT INTO quiz_attempts (user_id, quiz_id, answer_one, answer_two, answer_three, answer_four, quiz_result) VALUES (1, 1, 3, 3, 1, 0, 4);
-INSERT INTO quiz_attempts (user_id, quiz_id, answer_one, answer_two, answer_three, answer_four, quiz_result) VALUES (2, 2, 0, 2, 3, 2, 3);
-INSERT INTO quiz_attempts (user_id, quiz_id, answer_one, answer_two, answer_three, answer_four, quiz_result) VALUES (3, 3, 0, 2, 0, 1, 2);
-INSERT INTO quiz_attempts (user_id, quiz_id, answer_one, answer_two, answer_three, answer_four, quiz_result) VALUES (4, 1, 0, 0, 1, 1, 1);
-INSERT INTO quiz_attempts (user_id, quiz_id, answer_one, answer_two, answer_three, answer_four, quiz_result) VALUES (5, 2, 0, 2, 0, 1, 3);
-INSERT INTO quiz_attempts (user_id, quiz_id, answer_one, answer_two, answer_three, answer_four, quiz_result) VALUES (1, 2, 0, 3, 1, 0, 2);
+INSERT INTO quiz_attempts (user_id, quiz_id, answer_one, answer_two, answer_three, answer_four, quiz_result) VALUES (1, 1, '22', '53', '443', 'None of the above', 4);
+INSERT INTO quiz_attempts (user_id, quiz_id, answer_one, answer_two, answer_three, answer_four, quiz_result) VALUES (2, 2, '1996', '1994', '1977', '1999', 3);
+INSERT INTO quiz_attempts (user_id, quiz_id, answer_one, answer_two, answer_three, answer_four, quiz_result) VALUES (3, 3, 'Toronto', 'Mexico', 'Total Recall', 'London', 2);
+INSERT INTO quiz_attempts (user_id, quiz_id, answer_one, answer_two, answer_three, answer_four, quiz_result) VALUES (4, 1, '22', '443', '25', '1337', 1);
+INSERT INTO quiz_attempts (user_id, quiz_id, answer_one, answer_two, answer_three, answer_four, quiz_result) VALUES (5, 2, '1996', '1994', '1999', '2023', 3);
