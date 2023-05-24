7e6e35cfab1a8dd1a001e00a80d41f5dc48f91c3
Anthony Chambers <ajfchambers@gmail.com>
2023-05-24 14:16:51 -0400

    Adjust seed data

--- db/seeds/05_quiz_attempts.sql
+++ db/seeds/05_quiz_attempts.sql
@@ -1,5 +1,5 @@
-INSERT INTO quiz_attempts (user_id, quiz_id, answer_one, answer_two, answer_three, answer_four, quiz_result) VALUES (1, 1, '22', '53', '443', 'None of the above', 4);
-INSERT INTO quiz_attempts (user_id, quiz_id, answer_one, answer_two, answer_three, answer_four, quiz_result) VALUES (2, 2, '1996', '1994', '1977', '1999', 3);
-INSERT INTO quiz_attempts (user_id, quiz_id, answer_one, answer_two, answer_three, answer_four, quiz_result) VALUES (3, 3, 'Toronto', 'Mexico', 'Total Recall', 'London', 2);
-INSERT INTO quiz_attempts (user_id, quiz_id, answer_one, answer_two, answer_three, answer_four, quiz_result) VALUES (4, 1, '22', '443', '25', '1337', 1);
-INSERT INTO quiz_attempts (user_id, quiz_id, answer_one, answer_two, answer_three, answer_four, quiz_result) VALUES (5, 2, '1996', '1994', '1999', '2023', 3);
+INSERT INTO quiz_attempts (user_id, quiz_id, quiz_result) VALUES (1, 1, 4);
+INSERT INTO quiz_attempts (user_id, quiz_id, quiz_result) VALUES (2, 2, 3);
+INSERT INTO quiz_attempts (user_id, quiz_id, quiz_result) VALUES (3, 3, 2);
+INSERT INTO quiz_attempts (user_id, quiz_id, quiz_result) VALUES (4, 1, 1);
+INSERT INTO quiz_attempts (user_id, quiz_id, quiz_result) VALUES (5, 2, 3);
---
+++ db/seeds/06_user_answers.sql
@@ -0,0 +1,24 @@
+INSERT INTO user_answers (quiz_attempt_id, answer_id) VALUES (1, 4);
+INSERT INTO user_answers (quiz_attempt_id, answer_id) VALUES (1, 8);
+INSERT INTO user_answers (quiz_attempt_id, answer_id) VALUES (1, 10);
+INSERT INTO user_answers (quiz_attempt_id, answer_id) VALUES (1, 16);
+
+INSERT INTO user_answers (quiz_attempt_id, answer_id) VALUES (2, 17);
+INSERT INTO user_answers (quiz_attempt_id, answer_id) VALUES (2, 23);
+INSERT INTO user_answers (quiz_attempt_id, answer_id) VALUES (2, 25);
+INSERT INTO user_answers (quiz_attempt_id, answer_id) VALUES (2, 28);
+
+INSERT INTO user_answers (quiz_attempt_id, answer_id) VALUES (3, 33);
+INSERT INTO user_answers (quiz_attempt_id, answer_id) VALUES (3, 38);
+INSERT INTO user_answers (quiz_attempt_id, answer_id) VALUES (3, 39);
+INSERT INTO user_answers (quiz_attempt_id, answer_id) VALUES (3, 45);
+
+INSERT INTO user_answers (quiz_attempt_id, answer_id) VALUES (4, 3);
+INSERT INTO user_answers (quiz_attempt_id, answer_id) VALUES (4, 8);
+INSERT INTO user_answers (quiz_attempt_id, answer_id) VALUES (4, 11);
+INSERT INTO user_answers (quiz_attempt_id, answer_id) VALUES (4, 15);
+
+INSERT INTO user_answers (quiz_attempt_id, answer_id) VALUES (5, 17);
+INSERT INTO user_answers (quiz_attempt_id, answer_id) VALUES (5, 23);
+INSERT INTO user_answers (quiz_attempt_id, answer_id) VALUES (5, 28);
+INSERT INTO user_answers (quiz_attempt_id, answer_id) VALUES (5, 32);
