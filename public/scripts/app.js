325254bdb2771ac0d8d98e042a55fe1417b83fb8
Anthony Chambers <ajfchambers@gmail.com>
2023-05-26 15:00:26 -0400

    Create styling for quiz

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,13 @@
 // Client facing scripts here
+/* eslint-env jquery */
+
+const selectAnswer = (button, index) => {
+  const questionBox = $(button).closest('.question-box');
+  const selectedButton = questionBox.find('button');
+  const userAnswer = $(`#a${index}`);
+
+  selectedButton.removeClass('selected');
+  $(button).addClass('selected');
+
+  userAnswer.val(button.value);
+};
