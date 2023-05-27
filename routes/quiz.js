d72f653ff7c3a2be7478ad0b0b6b2024c7f5129f
Anthony Chambers <ajfchambers@gmail.com>
2023-05-27 10:30:48 -0400

    Add login requirement

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,97 +1,126 @@
 const express = require('express');
 const router = express.Router();
 const { createQuizArray, insertQuizAttempt, loadCorrectAnswers, findAnswerId, insertUserAnswer, addQuizResult, quizExistCheck } = require('../db/queries/quiz');
+const { validUserCheck } = require('../db/queries/login');
 
 
 // Display quiz to be attempted based on params
 router.get('/:quiz_id', (req, res) => {
 
-  // Check if a quiz exists with the param id
-  quizExistCheck(req.params.quiz_id)
+  // Check if user is logged in with valid user id
+  validUserCheck(req.session.userId)
     .then(exists => {
       if (!exists) {
-        res.redirect('/home');
-        return;
+        res.redirect('/users/login');
       }
+    })
+    .then(() => {
 
-      // Create an array of questions and answers for the quiz
-      return createQuizArray(req.params.quiz_id)
-        .then(data => {
-          let templateVars = {
-            questions: data,
-            quizTitle: data[0].title,
-            quizId: req.params.quiz_id
-          };
-          res.render('../views/quiz', templateVars);
-        })
-        .catch(err => {
-          console.error('Error creating quiz and answer array: ', err);
-          res.status(500).send('Internal Server Error');
+      // Check if a quiz exists with the param id
+      quizExistCheck(req.params.quiz_id)
+        .then(exists => {
+          if (!exists) {
+            res.redirect('/home');
+            return;
+          }
+
+          // Create an array of questions and answers for the quiz
+          return createQuizArray(req.params.quiz_id)
+            .then(data => {
+              let templateVars = {
+                questions: data,
+                quizTitle: data[0].title,
+                quizId: req.params.quiz_id
+              };
+              res.render('../views/quiz', templateVars);
+            })
+            .catch(err => {
+              console.error('Error creating quiz and answer array: ', err);
+              res.status(500).send('Internal Server Error');
+            });
         });
+    })
+    .catch(err => {
+      console.error('Error checking if user is valid: ', err);
+      res.status(500).send('Internal Server Error');
     });
 });
 
 
 // End point to handle quiz submission
 router.post('/:quiz_id', (req, res) => {
-  const quizId = req.params.quiz_id;
-  const userId = req.session.userId;
 
-  let score = 0;
-  let answerId = null;
-  let quizResultId = 0;
+  validUserCheck(req.session.userId)
+    .then(exists => {
+      if (!exists) {
+        res.status(401).send('Must be logged in to submit a quiz');
+      }
+    })
+    .then(() => {
+
+      const quizId = req.params.quiz_id;
+      const userId = req.session.userId;
+
+      let score = 0;
+      let answerId = null;
+      let quizResultId = 0;
 
 
-  // Create an initial column in quiz_attempts table for the quiz attempt
-  insertQuizAttempt(quizId, userId, score)
-    .then(data => {
-      quizResultId = data[0].id;
-
-      // Load correct answers to quiz question
-      return loadCorrectAnswers(quizId)
+      // Create an initial column in quiz_attempts table for the quiz attempt
+      insertQuizAttempt(quizId, userId, score)
         .then(data => {
+          quizResultId = data[0].id;
 
-          const correctAnswers = data;
-          const submittedAnswers = req.body.a;
+          // Load correct answers to quiz question
+          return loadCorrectAnswers(quizId)
+            .then(data => {
 
-          for (let i = 0; i < correctAnswers.length; i++) {
+              const correctAnswers = data;
+              const submittedAnswers = req.body.a;
 
-            // Find answer id from submitted answer
-            findAnswerId(submittedAnswers[`${i}`])
-              .then(data => {
-                if (data[0] === undefined) {
-                  answerId = null;
-                } else {
-                  answerId = data[0].id;
-                }
-                // Insert user answer_id into user_answers table
-                insertUserAnswer(quizResultId, answerId)
+              for (let i = 0; i < correctAnswers.length; i++) {
+
+                // Find answer id from submitted answer
+                findAnswerId(submittedAnswers[`${i}`])
+                  .then(data => {
+                    if (data[0] === undefined) {
+                      answerId = null;
+                    } else {
+                      answerId = data[0].id;
+                    }
+                    // Insert user answer_id into user_answers table
+                    insertUserAnswer(quizResultId, answerId)
+                      .catch(err => {
+                        console.error('Error inserting answer_id to user_answer table: ', err);
+                        res.status(500).send('Internal Server Error');
+                      });
+                  })
                   .catch(err => {
-                    console.error('Error inserting answer_id to user_answer table: ', err);
+                    console.error('Error finding answer id: ', err);
                     res.status(500).send('Internal Server Error');
                   });
-              })
-              .catch(err => {
-                console.error('Error finding answer id: ', err);
-                res.status(500).send('Internal Server Error');
-              });
-            if (correctAnswers[i].answer === submittedAnswers[`${i}`]) {
-              score++;
-            }
-          }
-          // Update score in quiz_attempts table
-          addQuizResult(quizResultId, userId, quizId, score)
-            .then(() => {
-              res.redirect(302, `/result/${quizResultId}`);
+                if (correctAnswers[i].answer === submittedAnswers[`${i}`]) {
+                  score++;
+                }
+              }
+              // Update score in quiz_attempts table
+              addQuizResult(quizResultId, userId, quizId, score)
+                .then(() => {
+                  res.redirect(302, `/result/${quizResultId}`);
+                });
+            })
+            .catch(err => {
+              console.error('Error loading correct answers: ', err);
+              res.status(500).send('Internal Server Error');
             });
         })
         .catch(err => {
-          console.error('Error loading correct answers: ', err);
+          console.error('Error inserting quiz attempt: ', err);
           res.status(500).send('Internal Server Error');
         });
     })
     .catch(err => {
-      console.error('Error inserting quiz attempt: ', err);
+      console.error('Error checking if user is valid: ', err);
       res.status(500).send('Internal Server Error');
     });
 });
