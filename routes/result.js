a6df1df5e2d70cdee29424ef149693b7dfc87789
Anthony Chambers <ajfchambers@gmail.com>
2023-05-29 16:06:26 -0400

    Convert to async/await

--- routes/result.js
+++ routes/result.js
@@ -3,42 +3,37 @@ const router = express.Router();
 const { getQuestionsCorrectAnswers, getUserProvidedAnswers } = require('../db/queries/results');
 
 // Find questions and the answers for a quiz by quiz_id
-router.get('/:quizResultId', (req, res) => {
+router.get('/:quizResultId', async(req, res) => {
+  try {
 
-  // Find the correct answers for questions
-  getQuestionsCorrectAnswers(req.params.quizResultId)
-    .then(data => {
-      if (data.length === 0) {
-        res.redirect('/home');
-        return;
-      }
+    // Check if quiz results exists, if so set data
+    const data = await getQuestionsCorrectAnswers(req.params.quizResultId);
+    if (data.length === 0) {
+      res.redirect('/home');
+      return;
+    }
 
-      const templateVars = {
-        quizzes: data,
-        quizResult: data[0].quiz_result,
-        quizTitle: data[0].title
-      };
+    const templateVars = {
+      quizzes: data,
+      quizResult: data[0].quiz_result,
+      quizTitle: data[0].title
+    };
 
-      // Find the provided user answers
-      getUserProvidedAnswers(req.params.quizResultId)
-        .then(providedAnswersData => {
+    // Assemble answers provided by user
+    const providedAnswersData = await getUserProvidedAnswers(req.params.quizResultId);
 
-          if (providedAnswersData === null) {
-            templateVars.answers = [];
-          } else {
-            templateVars.answers = providedAnswersData[0].answers;
-          }
+    //console.log('providedAnswersData: ', providedAnswersData);
 
-          res.render('../views/result', templateVars);
-        })
-        .catch(err => {
-          console.error('Error loading user provided answers: ', err);
-          res.status(500).send('Internal Server Error');
-        });
-    })
-    .catch(err => {
-      console.error('Error loading correct answers: ', err);
-      res.status(500).send('Internal Server Error');
-    });
+    if (providedAnswersData === null) {
+      templateVars.answers = [];
+    } else {
+      templateVars.answers = providedAnswersData[0].answers;
+    }
+
+    res.render('../views/result', templateVars);
+  } catch (err) {
+    console.error('Error loading quiz result: ', err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 module.exports = router;
