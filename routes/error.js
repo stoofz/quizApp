f13b3614a80a6f6d7353052d0a4d3d49972d3d93
Anthony Chambers <ajfchambers@gmail.com>
2023-05-30 13:05:49 -0400

    Add error handling

--- routes/error.js
+++ routes/error.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/error', (req, res) => {
-  const errorMessage = decodeURIComponent(req.query.message);
-  res.render('error', { message: errorMessage });
+  try {
+    const errorMessage = decodeURIComponent(req.query.message);
+    res.render('error', { message: errorMessage });
+  } catch (err) {
+    console.error('Error rendering error page: ', err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 module.exports = router;
--- routes/logout.js
+++ routes/logout.js
@@ -3,9 +3,14 @@ const router = express.Router();
 
 // Clears session cookie
 router.get('/logout', (req, res) => {
-  req.session = null;
-  res.clearCookie('session');
-  res.redirect('/users/login');
+  try {
+    req.session = null;
+    res.clearCookie('session');
+    res.redirect('/users/login');
+  } catch (err) {
+    console.error('Error clearing session: ', err);
+    res.redirect(302, `/error?message=${encodeURIComponent('Internal Server Error')}`);
+  }
 });
 
 module.exports = router;
