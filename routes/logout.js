5d48c92cefd7a302ea55c4b70b4543dc9309a49f
Saim Amir <saimxamir@gmail.com>
2023-05-30 23:37:49 -0400

    update login redirect

--- routes/logout.js
+++ routes/logout.js
@@ -7,11 +7,7 @@ router.post('/logout', async (req, res) => {
   try {
     req.session = null;
     res.clearCookie('session');
-    const userInfo = await getUserInfo(req.session.userId);
-    const templateVars = {
-      userInfo,
-    };
-    res.redirect('/users/login',templateVars);
+    res.redirect('/');
   } catch (err) {
     console.error('Error clearing session: ', err);
     res.redirect(302, `/error?message=${encodeURIComponent('Internal Server Error')}`);
