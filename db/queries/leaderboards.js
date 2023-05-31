c25f7604a0be8811054d31659fe211af34c0e209
Anthony Chambers <ajfchambers@gmail.com>
2023-05-31 12:51:09 -0400

    Add links

--- db/queries/leaderboards.js
+++ db/queries/leaderboards.js
@@ -38,10 +38,10 @@ const mostTaken = function() {
 
 const mostPopular = function() {
   return db.query(`
-  SELECT title, COUNT(quiz_id) AS count
+  SELECT title, COUNT(quiz_id) AS count, quizzes.id
   FROM quizzes
   JOIN quiz_attempts ON quizzes.id = quiz_attempts.quiz_id
-  GROUP BY title
+  GROUP BY title, quizzes.id
   ORDER BY count DESC
   LIMIT 5;`)
     .then(data => {
@@ -55,11 +55,11 @@ const mostPopular = function() {
 
 const mostDifficult = function() {
   return db.query(`
-  SELECT title, COUNT(quiz_attempts.quiz_id) AS count, ROUND((AVG(quiz_attempts.quiz_result) / COUNT(DISTINCT quiz_questions.question)) * 100) AS avg_percentage
+  SELECT title, COUNT(quiz_attempts.quiz_id) AS count, ROUND((AVG(quiz_attempts.quiz_result) / COUNT(DISTINCT quiz_questions.question)) * 100) AS avg_percentage, quizzes.id
   FROM quizzes
   JOIN quiz_attempts ON quizzes.id = quiz_attempts.quiz_id
   JOIN quiz_questions ON quizzes.id = quiz_questions.quiz_id
-  GROUP BY title
+  GROUP BY title, quizzes.id
   HAVING COUNT(quiz_attempts.quiz_id) >= 5
   ORDER BY avg_percentage ASC
   LIMIT 5;`)
