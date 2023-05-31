42fc7be2b1d0bc085a0ed8801f65708ef80d4fc4
Saim Amir <saimxamir@gmail.com>
2023-05-31 00:22:24 -0400

    add quizhistory to navbar

--- routes/quizhistory.js
+++ routes/quizhistory.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getQuizHistoryforUser } = require('../db/queries/quizhistory');
 const { validUserCheck } = require('../db/queries/login');
+const { getUserById } = require('../db/queries/userinfo.js')
 
 // Display quiz history for a specific user
 router.get('/:user_id', (req, res) => {
@@ -13,19 +14,24 @@ router.get('/:user_id', (req, res) => {
         res.redirect('/users/login');
         return;
       }
-    })
-    .then(() => {
-      const userId = req.params.user_id;
 
+      // Fetch user information
+      return getUserById(req.session.userId);
+    })
+    .then((user) => {
+      const userId = req.params.user_id;
 
       // Fetch quiz history for the user
       getQuizHistoryforUser(userId)
         .then(data => {
           if (data.length === 0) {
-            res.render('../views/quizhistory', { results: [] }); // Empty array for no quiz results
+            res.render('../views/quizhistory', { results: [] }, user); // Empty array for no quiz results
           } else {
-            const templateVars = { results: data };
+
+            // Add user object for _header.ejs conditionals
+            const templateVars = { results: data, user};
             res.render('../views/quizhistory', templateVars);
+            
           }
         })
         .catch(err => {
