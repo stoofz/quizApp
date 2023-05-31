e1b4347dff263a8293b233d867724bada6b97555
Anthony Chambers <ajfchambers@gmail.com>
2023-05-31 14:37:33 -0400

    Add share link

--- routes/result.js
+++ routes/result.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getQuestionsCorrectAnswers, getUserProvidedAnswers } = require('../db/queries/results');
-const { getUserById } = require('../db/queries/userinfo.js')
+const { getUserById } = require('../db/queries/userinfo.js');
 
 
 // Find questions and the answers for a quiz by quiz_id
@@ -17,6 +17,7 @@ router.get('/:quizResultId', async(req, res) => {
       quizzes: data,
       quizResult: data[0].quiz_result,
       quizTitle: data[0].title,
+      req,
       user
     };
 
